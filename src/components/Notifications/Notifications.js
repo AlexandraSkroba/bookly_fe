ba82b4821cc3313fce73ad210d0c21428b3655e2
frgpp <frgpp@users.noreply.github.com>
2025-01-14 12:32:32 +0300

    debug

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -22,6 +22,7 @@ export const Notifications = () => {
   }
 
   const fetchNotifications = async () => {
+    console.log('Fetching notifications', defaultHeaders)
     try {
       const response = await axios.get(API_ENDPOINTS.getNotifications, { headers: defaultHeaders });
       setNotifications(response.data);
